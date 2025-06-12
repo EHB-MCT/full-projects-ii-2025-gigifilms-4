88d29a681789f5fd4f60355fdbbbd0c83f4ce766
Diego <diegoburgo2005@gmail.com>
2025-06-12 16:07:33 +0200

    added the shortcuts naar de verschillende delen op de casting page

--- js/index.js
+++ js/index.js
@@ -1,2 +1,24 @@
+Init();
 
-console.log("hello world")
+function Init() {
+    const roleButton = document.getElementById('role');
+    const actorButton = document.getElementById('actor');
+    const profileButton = document.getElementById('profile');
+    if (roleButton) {
+        roleButton.addEventListener('click',function() {
+            window.scrollTo({ left: 1550, behavior: 'smooth'})
+        })
+    }
+    
+    if (actorButton) {
+        actorButton.addEventListener('click',function() {
+            window.scrollTo({ left: 2550, behavior: 'smooth'})
+        })
+    }
+    if (profileButton) {
+        profileButton.addEventListener('click',function() {
+            window.scrollTo({ left: 3550, behavior: 'smooth'})
+        })
+    }
+
+}
